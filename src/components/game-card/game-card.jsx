e747d727ef3e68346b8a050f0a8a3d64469a47d7
Your Name <you@example.com>
2022-02-16 23:28:24 +0200

    GS/gamePage: create game page and order page

--- src/components/game-card/game-card.jsx
+++ src/components/game-card/game-card.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import { GameCover } from '../game-cover';
 import { GameBuy } from '../game-buy';
 import { GameGenre } from '../game-genre';
 
+import { setCurrentGame } from '../../redux/games/reducer';
+
 import './game-card.css';
 
 export const GameCard = ({ game }) => {
+  const history = useHistory(); // это было в старой версии react-router, так что в финальном видео всё равно переделаю
+  const dispatch = useDispatch();
+
+  const PATH = `/app/${game.title.toLowerCase()}`;
+
+  const handleClick = () => {
+    dispatch(setCurrentGame(game));
+    history.push(PATH);
+  };
+
   return (
-    <div className="game-item">
+    <div onClick={handleClick} className="game-item">
       <GameCover image={game.image} />
       <div className="game-item__details">
         <span className="game-item__title">{game.title}</span>
+        {/* вынести код в отдельный компонент (мб, GameGenresList) 16-20 строки */}
         <div className="game-item__genre">
           {game.genres.map((genre) => (
             <GameGenre genre={genre} key={genre} />
